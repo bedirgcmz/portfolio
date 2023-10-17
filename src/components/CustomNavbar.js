74979845f1a9c77697bd8b4f37bc0cf66d1be644
bedirgcmz <bdrgcmz@gmail.com>
2023-10-17 17:01:19 +0200

    Bugs fixed

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,49 +1,66 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Navbar.css'; 
-import './hovers.css'; 
-import './Download.css'; 
-import img1 from "../images/img2.jpg"
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Navbar.css";
+import "./hovers.css";
+import "./Download.css";
+import img1 from "../images/img2.jpg";
+import Bedir_Gocmez_Cv from "../documents/Bedir_Gocmez_Cv.pdf";
 
 const CustomNavbar = () => {
   return (
-      
-    <Navbar className='px-3' expand="lg" fixed="top">
-        <Navbar.Brand className='brand-container' href="#about">
-          <img
-            src={img1}
-            width="40"
-            height="40"
-            className="d-inline-block align-top"
-            alt="Profile Image"
-          />
-          <div className='name-and-title ms-3 me-5 text-center'>
-            <p className='name text-white'>Bedir Gocmez</p> 
-            <p className="title">Fullstack Web Developer</p>
-          </div>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link clasName="hover-3 active" href="#about">About</Nav.Link>
-            <Nav.Link clasName="hover-3" href="#projects">Projects</Nav.Link>
-            <Nav.Link clasName="hover-3" href="#skills">Skills</Nav.Link>
-            <Nav.Link clasName="hover-3" href="#experiences">Experiences</Nav.Link>
-            <Nav.Link clasName="hover-3" href="#educations">Educations</Nav.Link>
-            <Nav.Link clasName="hover-3" href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <a href='www.google.com' target='_blank' className='download-cv float-right btn-one'>
-            <span ><i class="fa-solid fa-download pe-2"></i>Download CV</span>        
-        </a>
-      </Navbar>
-   
+    <Navbar className="px-3" expand="lg" fixed="top">
+      <Navbar.Brand className="brand-container" href="#about">
+        <img
+          src={img1}
+          width="40"
+          height="40"
+          className="d-inline-block align-top"
+          alt="Profile Image"
+        />
+        <div className="name-and-title ms-3 me-5 text-center">
+          <p className="name text-white">Bedir Gocmez</p>
+          <p className="title">Fullstack Web Developer</p>
+        </div>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link clasName="hover-3 active" href="#about">
+            About
+          </Nav.Link>
+          <Nav.Link clasName="hover-3" href="#projects">
+            Projects
+          </Nav.Link>
+          <Nav.Link clasName="hover-3" href="#skills">
+            Skills
+          </Nav.Link>
+          <Nav.Link clasName="hover-3" href="#experiences">
+            Experiences
+          </Nav.Link>
+          <Nav.Link clasName="hover-3" href="#educations">
+            Educations
+          </Nav.Link>
+          <Nav.Link clasName="hover-3" href="#contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      <a
+        href={Bedir_Gocmez_Cv}
+        target="_blank"
+        className="download-cv float-right btn-one"
+        download="Bedir-Gocmez-CV.pdf"
+      >
+        <span>
+          <i class="fa-solid fa-download pe-2"></i>Download CV
+        </span>
+      </a>
+    </Navbar>
   );
-}
+};
 
 export default CustomNavbar;
-
 
 // import React from "react";
 // import "./Navbar.css";
